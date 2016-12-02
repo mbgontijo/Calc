ad9ece7946a7d7e2d0cab78f807463ffc92e095a
Mauricio <mauricio.gontijo@outlook.com>
2016-12-02 12:26:25 -0200

    Primeiro commit

---
+++ GitCalc/GitCalc/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCalc
+{
+    class Calc
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
